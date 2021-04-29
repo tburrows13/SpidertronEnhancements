7b775e5efcccb5bd9e4bfddf6705ed5e8c9a63b6
Tom Burrows <tomburrows13@icloud.com>
2021-04-29 02:02:41 +0100

    Rearrange open-inventory, open cursor_stack.connected_entity

--- scripts/open-inventory.lua
+++ scripts/open-inventory.lua
@@ -1,14 +1,19 @@
 script.on_event("spidertron-enhancements-open-vehicle-inventory",
   function(event)
     local player = game.get_player(event.player_index)
-    local vehicle = player.vehicle
-    if vehicle and vehicle.valid then
-      if player.opened then
-        -- Close the GUI
-        player.opened = nil
+    if player.opened_gui_type == defines.gui_type.none then
+      local cursor_stack = player.cursor_stack
+      if cursor_stack and cursor_stack.valid_for_read and cursor_stack.type == "spidertron-remote" and cursor_stack.connected_entity then
+        player.opened = cursor_stack.connected_entity
       else
-        player.opened = vehicle
+        local vehicle = player.vehicle
+        if vehicle and vehicle.valid then
+          player.opened = vehicle
+        end
       end
+    else
+      -- Close the GUI
+      player.opened = nil
     end
   end
 )
