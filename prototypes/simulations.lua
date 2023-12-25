f6ff4ca0c920d14572394054ac4ebcd2d98387b6
Tom Burrows <tomburrows13@icloud.com>
2023-12-25 01:22:20 +0000

    Remove simhelper funccapture usage

--- prototypes/simulations.lua
+++ prototypes/simulations.lua
@@ -1,18 +1,4 @@
 if mods["simhelper"] and not mods["SpidertronEngineer"] then
-  local func_capture = require("__simhelper__.funccapture")
-  local math2d = require("__core__.lualib.math2d")
-
-  local function fill_tiles(left_top, right_bottom, tile)
-    local tiles = {}
-    for i = left_top[1], right_bottom[1] do
-      for j = left_top[2], right_bottom[2] do
-        table.insert(tiles, {position = {i, j}, name = tile})
-      end
-    end
-    game.surfaces[1].set_tiles(tiles)
-
-  end
-
   data:extend{
     {
       type = "tips-and-tricks-item",
@@ -33,7 +19,8 @@ if mods["simhelper"] and not mods["SpidertronEngineer"] then
       trigger = {type = "build-entity", entity = "spidertron", match_type_only = true},
       simulation = {
         save = "__SpidertronEnhancements__/simulations/SpidertronEnhancementsSim.zip",
-        init = func_capture.capture(function()
+        init =
+        [[
           local bp="0eNq9mdtu2zAMht9F185gijrYud4b7HIoCtfxUmGOHfjQrijy7pPipGkzFiExbDctFNufSVG/SFOv6qGdm/0QukmtX1Wo+25U6++vagzbrmrTb9PLvlFrFaZmpzLVVbs0GqrQqkOmQrdpfqk1HO4y1XRTmEKzPH8cvNx38+6hGeINb0/W8/DUbFZHQKb2/Rif6bv0osjRmXpR6xUUEb0JQ1Mv19wh+4OoeUTIaSQSSHxDjlOkbR+nT6ArOEHtR6gnoIYLBaShQEAtz/mVOSH1baRj22lpOy0B9WIo3ra0YEM9DaUsLcVQuG1pWnxMaklTKVMBpNSSYSpTT/asp/y2RAGZzIJmUhoFtp40kM6TM2qlUM+YUbaijCeh1G4CbEU5gaUXRbV93e/6KTw1xG5y0qiJxH4IEVItV+OSqPu2H9J9w3G4jX+/aLCucNo7a23uAE2OqHMEHTkPx5RQpdtMWeRYxpvjPzAOncbcuMKn7SUlnTFhu7lum2pY/ZibNi0Mygu2hM+OeAqjL5pNlG41Tv2eYOBlesfzRKhv/TzUaer+yXxQ1rL3Ao3keqDkoLUUykhZmp1djSWhlBy0kUL97S1GM7OrM2c9UBAnXY10fNl61/SskfEtpFBGVtZs+RkkoVR8MZdCLaPMA2loyP0G+SrxpMNUaJCZMnFJwzljCg2TWFBETRHZ2dIA6TgZFLFegAzKRS91NWz71XO1jU9+BkEilYXuKY77IV7u5ralXlIIbaVNLUXbjLm9eEwutItc10YqD3LjMloUiGPpJ4+EQaGtZMo3RlSnAkMjxgrrNEZeMlKJlKSznlXf6PKE+Fjlvy92vjbjFLpl9P8qHiMWH6k+w1SfXtR3/QVNfe5YtvxOk3v9sUctJMtWIwIfyk5diHwoW4to+VB2gYeeD2WLEwWBcsKsyIJ6Yf3DghbCopkFLYVfuxyo45d//EA5tqIsP1COrSjLD5STZjcgE7qTtkw4bU0na0Fet18NhXTCvhYwGibOS6GMLwgnbUFCybBU2oIERs3gc2EHVsNtSz0Iodfuk5ZqYfddM1qlHiXnDtowfDeic4drJCVzb4VHBNeeYzp9GevHZjO3p+OXSx8vjaF4d8NyvPNny+q5CtN93Xeb4xsXTITsq6G5P50BxQIvO58HTWGXnppC/XNMO/XhLrn2WXX49/C75OTxEGr97swqU0/NMC5TW4Dxpfa61AjoDoff7kG9Jg=="
           game.surfaces[1].create_entities_from_blueprint_string{string = bp, position = {0, 0}}
 
@@ -103,7 +90,7 @@ if mods["simhelper"] and not mods["SpidertronEngineer"] then
               log("Changed state from " .. event.old_state)
             end
           )
-        end)
+        ]]
       }
     },
     {
@@ -116,7 +103,8 @@ if mods["simhelper"] and not mods["SpidertronEngineer"] then
       trigger = {type = "build-entity", entity = "spidertron", match_type_only = true},
       simulation = {
         save = "__SpidertronEnhancements__/simulations/SpidertronEnhancementsSim.zip",
-        init = func_capture.capture(function()
+        init =
+        [[
           local spidertron = game.surfaces[1].create_entity{name = "spidertron", position = {-0.5, 1.5}, force = "player"}
           spidertron.color = {0, 1, 1, 0.5}
           --spidertron.torso_orientation = 0.4
@@ -133,7 +121,7 @@ if mods["simhelper"] and not mods["SpidertronEngineer"] then
               remote.call("SpidertronEnhancementsInternal-hs", "enter-vehicles", player)
             end
           end)
-        end)
+        ]]
       }
     },
     {
@@ -146,7 +134,18 @@ if mods["simhelper"] and not mods["SpidertronEngineer"] then
       trigger = {type = "build-entity", entity = "spidertron", match_type_only = true},
       simulation = {
         save = "__SpidertronEnhancements__/simulations/SpidertronEnhancementsSim.zip",
-        init = func_capture.capture(function()
+        init =
+        [[
+          local function fill_tiles(left_top, right_bottom, tile)
+            local tiles = {}
+            for i = left_top[1], right_bottom[1] do
+              for j = left_top[2], right_bottom[2] do
+                table.insert(tiles, {position = {i, j}, name = tile})
+              end
+            end
+            game.surfaces[1].set_tiles(tiles)
+          end
+        
           local spidertron = game.surfaces[1].create_entity{name = "spidertron", position = {-26, 0}, force = "player"}
           spidertron.color = {1, 0, 0, 0.5}
           --spidertron.torso_orientation = 0.4
@@ -170,7 +169,6 @@ if mods["simhelper"] and not mods["SpidertronEngineer"] then
           -- Generate water tiles
           fill_tiles({-19, -70}, {-5, 10}, "water")
           fill_tiles({5, -10}, {19, 70}, "water")
-
 
           destinations = {{26, 0}, {-26, 0}}
           step_1 = function()
@@ -214,7 +212,7 @@ if mods["simhelper"] and not mods["SpidertronEngineer"] then
           end
 
           step_1()
-        end)
+        ]]
       }
     },
     {
@@ -227,10 +225,16 @@ if mods["simhelper"] and not mods["SpidertronEngineer"] then
       trigger = {type = "build-entity", entity = "spidertron", match_type_only = true},
       simulation = {
         save = "__SpidertronEnhancements__/simulations/SpidertronEnhancementsSim.zip",
-        init = func_capture.capture(function()
+        init =
+        [[
+          function get_centre(box) -- copied from math2d
+            local x = box.left_top.x + (box.right_bottom.x - box.left_top.x) / 2
+            local y = box.left_top.y + (box.right_bottom.y - box.left_top.y) / 2
+            return {x = x, y = y}
+          end
           local spidertron = game.surfaces[1].create_entity{name = "spidertron", position = {3, 1.5}, force = "player"}
           spidertron.color = {0, 0.4, 1, 0.5}
-          local pipette_position = math2d.bounding_box.get_centre(spidertron.selection_box)
+          local pipette_position = get_centre(spidertron.selection_box)
           --spidertron.torso_orientation = 0.4
 
           local player = game.create_test_player{name = "character"}
@@ -304,7 +308,7 @@ if mods["simhelper"] and not mods["SpidertronEngineer"] then
           end
 
           step_1()
-        end)
+        ]]
       }
     },
   }
