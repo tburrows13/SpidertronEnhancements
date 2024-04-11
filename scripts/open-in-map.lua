274a05c3f5858f5731a40a83f3ca99001a0033f7
Tom Burrows <tomburrows13@icloud.com>
2024-04-11 17:09:19 +0100

    Fix crash when opening in map on different surface, open in SE navsat if available

--- scripts/open-in-map.lua
+++ scripts/open-in-map.lua
@@ -3,8 +3,21 @@ script.on_event("spidertron-enhancements-open-in-map",
     local player = game.get_player(event.player_index)
     local cursor_stack = player.cursor_stack
     if cursor_stack and cursor_stack.valid_for_read and cursor_stack.type == "spidertron-remote" and cursor_stack.connected_entity then
-      local vehicle = cursor_stack.connected_entity
-      player.open_map(vehicle.position, (1/16) * player.display_scale, vehicle)  
+      local spidertron = cursor_stack.connected_entity
+      if player.surface == spidertron.surface then
+        player.open_map(spidertron.position, (1/16) * player.display_scale, spidertron)
+      else
+        -- Player and spidertron are on different surfaces - try using SE navsat
+        if remote.interfaces["space-exploration"] and remote.interfaces["space-exploration"]["remote_view_is_unlocked"] and
+          remote.call("space-exploration", "remote_view_is_unlocked", { player = player }) then
+          surface_name = spidertron.surface.name:gsub("^%l", string.upper)
+          remote.call("space-exploration", "remote_view_start", {player = player, zone_name = surface_name, position = spidertron.position})
+          if remote.call("space-exploration", "remote_view_is_active", { player = player }) then
+            -- remote_view_start worked
+            player.close_map()
+          end
+        end
+      end
     end
   end
 )
