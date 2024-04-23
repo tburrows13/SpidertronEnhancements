e59633497d503bb69f301ee780ad2c58d29ffe44
Tom Burrows <tomburrows13@icloud.com>
2024-04-23 01:51:00 +0100

    Remove RemoteConfiguration compatibility

--- scripts/open-inventory.lua
+++ scripts/open-inventory.lua
@@ -16,38 +16,3 @@ script.on_event("spidertron-enhancements-open-vehicle-inventory",
     end
   end
 )
-
--- Open the spidertron by clicking on it even if the player doesn't enable "interact in game"
-if script.active_mods["RemoteConfiguration"] then
-  script.on_event("rc-open-gui",
-    function(event)
-      local player = game.get_player(event.player_index)
-      if player.mod_settings["rc-interact-in-game"].value then return end
-      local cursor_stack = player.cursor_stack
-      if (cursor_stack and cursor_stack.valid_for_read) or player.cursor_ghost then return end
-      local selected = player.selected
-      if selected and selected.type == "spider-vehicle" then
-        remote.call("RemoteConfiguration", "open_entity", player, selected)
-      end
-    end
-  )
-end
-
-script.on_event(defines.events.on_gui_closed,
-  function(event)
-    local spidertron = event.entity
-    if spidertron and spidertron.type == "spider-vehicle" then
-      local player = game.get_player(event.player_index)
-      -- If still in range, assume that it was closed deliberately
-      if remote.interfaces["RemoteConfiguration"] then
-        if not remote.call("RemoteConfiguration", "reset_this_tick", player) and not player.can_reach_entity(spidertron) then
-          remote.call("RemoteConfiguration", "open_entity", player, spidertron)
-        end
-      else
-        if not player.can_reach_entity(spidertron) then
-          player.print({"cursor-message.spidertron-enhancements-remote-configuration-required"})
-        end
-      end
-    end
-  end
-)
