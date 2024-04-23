952ebf66a585794c4ef68074861bd39b6326245f
Tom Burrows <tomburrows13@icloud.com>
2024-04-23 02:30:02 +0100

    Further 2.0 shortcut icon fixes

--- prototypes/shortcuts.lua
+++ prototypes/shortcuts.lua
@@ -6,7 +6,10 @@ local recall_shortcut = {
   toggleable = true,
   order = "d[spidertron-enhancements]",
   icon = "__SpidertronEnhancements__/graphics/follow-shortcut.png",
+  icon_size = 32,
+  icon_mipmaps = 1,
   small_icon = "__SpidertronEnhancements__/graphics/follow-shortcut-24.png",
+  small_icon_size = 24,
 }
 
 local recall_input = {
