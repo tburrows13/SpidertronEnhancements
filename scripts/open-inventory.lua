33b386c93b360eafedd9c38065e1c6b4bcda7e97
Tom Burrows <tomburrows13@icloud.com>
2024-11-27 03:11:13 +0000

    Improve remotely-open inventory code to only open inventory if out of reach

--- scripts/open-inventory.lua
+++ scripts/open-inventory.lua
@@ -29,29 +29,25 @@ script.on_event("spidertron-enhancements-open-gui",
     local player = game.get_player(event.player_index)
     if not player.is_cursor_empty() then return end
     local selected = player.selected
-    if selected and selected.type ~= "spider-vehicle" then
-      selected = nil
-    end
-
-    if not selected then
+    if selected and selected.type == "spider-vehicle" then
+      if not player.can_reach_entity(selected) then
+        player.set_controller{
+          type = defines.controllers.remote,
+          position = player.position,
+        }
+        player.opened = selected
+      end
+    else
       -- Try from the map for trains (and other vehicles)
       if player.render_mode == defines.render_mode.chart and not player.opened then
         -- Don't need to check chart_zoomed_in because spidertrons have radars, so would be selectable
         local position = event.cursor_position
         local vehicles = player.surface.find_entities_filtered{type = {"locomotive", "spider-vehicle"}, position = position, radius = 4.5, limit = 1}
         if #vehicles > 0 then
-          selected = vehicles[1]
+          player.opened = vehicles[1]
+          return
         end
       end
-    end
-    if selected then
-      if not player.can_reach_entity(selected) then
-        player.set_controller{
-          type = defines.controllers.remote,
-          position = player.position,
-        }
-      end
-      player.opened = selected
     end
   end
 )
